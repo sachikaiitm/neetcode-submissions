49cba74735a218fd53d2c6d131168131adf08203
sachikaiitm <sachikajay@gmail.com>
2026-05-29 16:11:27 +0530

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalin(const string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++; r--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return isPalin(s, l + 1, r) || isPalin(s, l, r - 1);
+            }
+            l++; r--;
+        }
+
+        return true;
+    }
+};
